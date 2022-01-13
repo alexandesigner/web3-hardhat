55a07284bffe31e979cf1a6296a7b32565beb543
Allan Alexandre <alexandesigner@gmail.com>
2022-01-13 00:03:53 -0300

    fix: version solidity

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,6 +1,6 @@
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
 
 // This is the main building block for smart contracts.
