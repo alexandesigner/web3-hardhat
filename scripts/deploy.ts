6bcbc8192385f7a3e35e61ccc4b77b3ccdb96429
Allan Alexandre <alexandesigner@gmail.com>
2022-01-19 18:36:37 -0300

    fix(deploy): add deploy file

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,6 +6,17 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+
+  console.log("Token address:", token.address);
+
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
   //
